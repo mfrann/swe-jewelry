74c5a56a86db6436c2e593a9b3fce90dae369d3d
Fran <mfranfc@gmail.com>
2025-11-21 19:14:37 -0500

    Header terminado

---
+++ src/components/header.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import "../styles/components/header.css";
+
+export function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <header className={`h-main-container ${isScrolled ? "scrolled" : ""}`}>
+      <div className="h-links">
+        <a href="#">Home</a>
+        <a href="#">Shop</a>
+        <a href="#">Press</a>
+      </div>
+      <div className="h-name">
+        <h1>
+          <span>S</span>we
+        </h1>
+      </div>
+      <div className="h-nav">
+        <a href="#">Search</a>
+        <a href="#">Cart(0)</a>
+        <a href="#">Login</a>
+      </div>
+    </header>
+  );
+}
---
+++ src/sections/questions.jsx
@@ -0,0 +1,3 @@
+export function Questions() {
+  return;
+}
